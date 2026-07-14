fcd529f05d42da4fa7cef953b5768d799245b8ae
souravcp <souravs@xgensys.biz>
2026-07-14 15:44:00 +0530

    test: add database provider and unit tests

---
+++ test/core/database/app_database_test.dart
@@ -0,0 +1,36 @@
+import 'package:drift/native.dart';
+import 'package:finman/core/database/app_database.dart';
+import 'package:finman/features/transaction/transaction_type.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  late AppDatabase database;
+
+  setUp(() {
+    database = AppDatabase(NativeDatabase.memory());
+  });
+
+  test('addCategory should insert a category', () async {
+    // arrange
+    final now = DateTime.now();
+    final category = CategoriesCompanion.insert(
+      name: 'Food',
+      type: TransactionType.expense,
+      icon: 'restaurant',
+      color: 0xFFF44336,
+      createdAt: now,
+      updatedAt: now,
+    );
+
+    // act
+    int id = await database.addCategory(category);
+    final categories = await database.getCategories();
+
+    // assert
+    expect(id, greaterThan(0));
+    expect(categories.length, 1);
+    expect(categories.first.name, 'Food');
+    expect(categories.first.type, TransactionType.expense);
+    expect(categories.first.icon, 'restaurant');
+  });
+}
